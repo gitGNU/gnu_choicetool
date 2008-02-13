098ef734abdf48933906633779355627e89c8255
Alessandro Massignan <ff0000.it@gmail.com>
2008-02-13 12:41:00 +0100

    Added data directory for site building

---
+++ site/index.scm
@@ -0,0 +1,24 @@
+(div (@ (id "main_body"))
+ (h3 "DESCRIPTION")
+ (p "Choicetool is a configuration management tool like "
+    (a (@ (href "http://www.catb.org/~esr/cml2/cml2-reference.html")) "CML2")
+    " or "
+    (a (@ (href "http://kernel.org/doc/#kconfig")) "Linux kernel kconfig")
+    "."
+    (br)
+    "It uses an input description in order to know the configuration symbols 
+     and their relations and creates a script that should be shipped with a 
+     package in order to let the user configure its options interactively."
+    (br) (br)
+    "It should work with (and even without) the autotools (like autotools'"
+    "libtool).")
+ (h3 "AUTHORS")
+ (p
+  "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+  (br)
+  "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
+ (h3 "RELEASE")
+  (p
+  "No public release yet... We're working hard :-)"
+  (br)
+  "Stay tuned!"))
