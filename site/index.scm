586a24af5c06e3d72eb7d2875b4db4043fb3ed2b
Francesco Salvestrini <salvestrini@gmail.com>
2008-02-15 23:12:23 +0100

    Updated site contents

--- site/index.scm
+++ site/index.scm
@@ -1,5 +1,4 @@
 (div (@ (id "main_body"))
-     (h3 "DESCRIPTION")
      (p "Choicetool is a configuration management tool like "
 	(a (@ (href "http://www.catb.org/~esr/cml2/cml2-reference.html"))
 	   "CML2")
@@ -8,18 +7,31 @@
 	   "Linux kernel kconfig")
 	"."
 	(br)
-	"It uses an input description in order to know the configuration symbols
-     and their relations and creates a script that should be shipped with a
-     package in order to let the user configure its options interactively."
+	"It uses an input description in order to know the configuration "
+	"symbols and their relations and creates a script that should be "
+	"shipped with a package in order to let the user be free to tweak "
+	"the configuration in its details without any fuss."
 	(br) (br)
-	"It should work with (and even without) the autotools (like autotools'"
-	"libtool).")
+	"It should work with (and even without) the autotools.")
+
      (h3 "AUTHORS")
-     (p
-      "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-      (br)
-      "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
+     (p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+        (br)
+        "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
+        )
+
      (h3 "RELEASE")
      (p
-      "No public release yet available yet...")
+      "Sorry, no public release available at the moment."
+      )
+
+     (h3 "DEVELOPMENT")
+     (p
+      "You can Browse the "
+      (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=choicetool.git"))
+         "Git repository")
+      " of this project with your web browser. This gives you a good picture "
+      "of the current status of the source files. You may also view the "
+      "complete histories of any file in the repository as well as "
+      "differences among two versions.")
      )
