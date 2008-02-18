2c1ce1b6dad43d80405ab5c90bc9e00929979f15
Alessandro Massignan <ff0000.it@gmail.com>
2008-02-18 09:21:08 +0100

    Updated and fixed site layout

--- site/index.scm
+++ site/index.scm
@@ -1,36 +1,30 @@
-(div (@ (id "main_body"))
-     (p "Choicetool is a configuration management tool like "
-	(a (@ (href "http://www.catb.org/~esr/cml2/cml2-reference.html"))
-	   "CML2")
-	" or "
-	(a (@ (href "http://kernel.org/doc/#kconfig"))
-	   "Linux kernel kconfig")
-	"."
-	"It uses an input description in order to know the configuration "
-	"symbols and their relations and creates a script that should be "
-	"shipped with a package in order to let the user be free to tweak "
-	"the configuration in its details without any fuss."
-	(br) (br)
-	"Choicetool should work with (and even without) the autotools.")
-
-     (h3 "AUTHORS")
-     (p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
-        (br)
-        "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
-        )
-
-     (h3 "RELEASE")
-     (p
-      "Sorry, no public release available at the moment."
-      )
-
-     (h3 "DEVELOPMENT")
-     (p
-      "You can Browse the "
-      (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=choicetool.git"))
-         "Git repository")
-      " of this project with your web browser. This gives you a good picture "
-      "of the current status of the source files. You may also view the "
-      "complete histories of any file in the repository as well as "
-      "differences among two versions.")
-     )
+(h3 "DESCRIPTION")
+(p "Choicetool is a configuration management tool like "
+   (a (@ (href "http://www.catb.org/~esr/cml2/cml2-reference.html"))
+      "CML2")
+   " or "
+   (a (@ (href "http://kernel.org/doc/#kconfig")) "Linux kernel kconfig")
+   "."
+   (br)
+   "It uses an input description in order to know the configuration 
+    symbols and their relations and creates a script that should be
+    shipped with a package in order to let the user be free to tweak
+    the configuration in its details without any fuss."
+   (br)
+   "Choicetool should work with (and even without) the autotools.")
+(h3 "AUTHORS")
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
+   (br)
+   "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>"
+ )
+(h3 "RELEASE")
+(p "Sorry, no public release available at the moment.")
+(h3 "DEVELOPMENT")
+(p "You can Browse the "
+   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=choicetool.git"))
+      "Git repository")
+   " of this project with your web browser. This gives you a good picture
+    of the current status of the source files. You may also view the
+    complete histories of any file in the repository as well as
+    differences among two versions."
+ )
