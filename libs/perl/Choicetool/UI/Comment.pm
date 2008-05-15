b0dc226cf58637c2ac180de1d0edfc4e323625ca
Francesco Salvestrini <salvestrini@gmail.com>
2008-05-15 18:45:18 +0200

    Subclassed UI Comment

--- libs/perl/Choicetool/UI/Comment.pm
+++ libs/perl/Choicetool/UI/Comment.pm
@@ -28,39 +28,43 @@ use diagnostics;
 
 use Choicetool::Base::Debug;
 use Choicetool::Base::Trace;
-use Choicetool::Data::Tree;
+use Choicetool::UI::UI;
 
-# Comment inherits from Tree
 use vars qw(@ISA);
-@ISA = qw(Choicetool::Data::Tree);
+@ISA = qw(Choicetool::UI::UI);
 
 sub new ($)
 {
     my $class = shift;
+    my $id    = shift;
 
     assert(defined($class));
 
-    my $self = { };
+    my $self = $class->SUPER::new($id);
 
     return bless($self, $class);
 }
 
-sub m4ify ($) {
+sub m4ify_header ($$) {
     my $self   = shift;
     my $prefix = shift;
 
     assert(defined($self));
     assert(defined($prefix));
+    assert(defined($self->id()));
 
-    my $string;
-    $string = "";
+    return $prefix . "_CT_UI_COMMENT_BEGIN([" . $self->id() . "])\n";
+}
 
-    $string = $string .
-	$prefix . "_CT_UI_COMMENT_BEGIN([" . $self->title() . "])\n";
-    $string = $string .
-	$prefix . "_CT_UI_COMMENT_END(["   . $self->title() . "])\n";
+sub m4ify_footer ($$) {
+    my $self   = shift;
+    my $prefix = shift;
 
-    return $string;
+    assert(defined($self));
+    assert(defined($prefix));
+    assert(defined($self->id()));
+
+    return $prefix . "_CT_UI_COMMENT_END([])\n";
 }
 
 1;
