f680df8815264cb73f76b86d25cedd85a7b19e61
Francesco Salvestrini <salvestrini@gmail.com>
2008-08-03 17:03:38 +0200

    Updated site related files

--- site/in/index.scm
+++ site/in/index.scm
@@ -14,17 +14,13 @@
    "Choicetool should work with (and even without) the "
    (a (@ (href "http://en.wikipedia.org/wiki/Autotools")) autotools)
    ".")
-(h4 "Copying")
-(p
- "Choicetool is licensed under the "
- (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
-    "GNU General Public License, version 2")
- )
+(h3 "COPYING")
+(p "Choicetool is licensed under the "
+   (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
+      "GNU General Public License, version 2"))
 
 (h3 "MAINTAINERS")
-(p
- "Francesco Salvestrini <salvestrini AT gmail DOT com>"
- )
+(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
 
 (h3 "AUTHORS")
 (p "Francesco Salvestrini <salvestrini AT gmail DOT com>"
@@ -36,38 +32,28 @@
 (p "Sorry, no public release available at the moment.")
 
 (h3 "MAILING LISTS")
-;(p
-; "Choicetool has several moderated mailing lists, each with an archive.
-; For general Choicetool discussions, use <choicetool-user AT nongnu.org>.
-; Email bug reports to <choicetool-bug AT nongnu.org>. For more information on
-; submitting bugs, please see the section Report a Bug below.
-; If you have a patch for a bug in Choicetool that hasn't yet been fixed in the
-; latest repository sources, please send the patch (made for the git sources,
-; not the release sources) to <choicetool-patch AT nongnu.org>."
-; )
-(p
- "Choicetool has several moderated mailing lists, each with an archive.
- For general Choicetool discussions, use <choicetool-user AT nongnu.org>.
- Email bug reports to <choicetool-bug AT nongnu.org>. For more information on
- submitting bugs, please see the section Report a Bug below.
- If you have a patch for a bug in Choicetool that hasn't yet been fixed in the
- latest repository sources, please send the patch (made for the git sources,
- not the release sources) to <choicetool-patch AT nongnu.org>."
- )
+(p "Choicetool has a single moderated mailing list, with an archive. "
+   "In order to post a message to the mailing list you must be subscribed. "
+   "Please consult the "
+   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/choicetool-generic") )
+      "Choicetool mailing list page")
+   " for more information on subscribing to the mailing list.")
 
 (h3 "REPORT A BUG")
-(p
- "If you think you have found a bug in Choicetool, then please send as complete
- a report as possible to <choicetool-bug AT nongnu.org>. An easy way to collect all
- the required information, such as platform and compiler, is to run make check,
- and include the resulting file tests/testsuite.log to your report.
- Disagreements between the manual and the code are also bugs."
- )
+(p "If you think you have found a bug in Choicetool then please send as "
+   "complete a report as possible to <choicetool AT nongnu.org>. An easy way "
+   "to collect all the required information, such as platform and compiler, "
+   "is to include in your report the config.log file available at the end of "
+   "the configuration procedure. ")
+(br)
+(p "If you have a patch for a bug in Choicetool that hasn't yet been fixed in "
+   "the latest repository sources, please be so kind to create it using the "
+   "repository sources, not the release sources.")
 
 (h3 "DEVELOPMENT")
 (h4 "Browsing sources")
 (p
- "You can Browse the "
+ "You can browse the "
  (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=choicetool.git"))
     "Git repository")
  " of this project with your web browser. This gives you a good
@@ -75,8 +61,20 @@
  the complete histories of any file in the repository as well as
  differences among two versions."
  )
-(h4 "Getting a Copy of the Git Repository")
+(h4 "Getting a copy of the Git Repository")
 (p "Anonymous checkout:")
 (br)
 (p (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
    " clone git://git.savannah.nongnu.org/choicetool.git")
+
+(h4 "Contribute")
+(p "If you have time and programming skills, you can help us by developing "
+   "missing features, regression tests or bug-fixing the present codebase. "
+   "Subscribe to the "
+   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/choicetool-generic") )
+      "mailing list")
+   ", drop us a mail and start coding. Send your code to the "
+   "mailing list under the form of patches for the current revision system.")
+(br)
+(p "If you have time but no programming skills, you can help with "
+   "documentation,  packaging, tests before releases etc ...")
