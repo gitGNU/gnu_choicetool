37bd00c8cc21de7d0e5f8c31d3ad10ad601ba110
Francesco Salvestrini <salvestrini@gmail.com>
2008-09-21 17:24:17 +0200

    Updated main site page

--- site/in/index.scm
+++ site/in/index.scm
@@ -18,7 +18,7 @@
    "is available.")
 
 (h3 "COPYING")
-(p "Choicetool is licensed under the "
+(p "The project is licensed under the "
    (a (@ (href "http://www.gnu.org/licenses/licenses.html"))
       "GNU General Public License, version 2"))
 
@@ -34,22 +34,23 @@
 (p "Sorry, no public release available at the moment.")
 
 (h3 "MAILING LISTS")
-(p "Choicetool has a single moderated mailing list, with an archive. "
+(p "The project has a single moderated mailing list, with an archive. "
    "In order to post a message to the mailing list you must be subscribed. "
    "Please consult the "
    (a (@ (href "http://lists.nongnu.org/mailman/listinfo/choicetool-generic"))
-      "Choicetool mailing list page")
+      "mailing list page")
    " for more information on subscribing to the mailing list.")
 
 (h3 "REPORT A BUG")
-(p "If you think you have found a bug in Choicetool then please send as "
-   "complete a report as possible to <choicetool AT nongnu DOT org>. An easy "
-   "way to collect all the required information, such as platform and "
+(p "If you think you have found a bug then please send as complete a report "
+   "as possible to "
+   "<choicetool-generic AT nongnu DOT org>. "
+   "An easy way to collect all the required information, such as platform and "
    "compiler, is to include in your report the config.log file available at "
    "the end of the configuration procedure. "
    (br)
    (br)
-   "If you have a patch for a bug in Choicetool that hasn't yet been fixed in "
+   "If you have a patch for a bug that hasn't yet been fixed in "
    "the latest repository sources, please be so kind to create it using the "
    "repository sources, not the release sources.")
 
@@ -65,7 +66,9 @@
 
 (h4 "Getting a copy of the Git Repository")
 (p "Anonymous checkout: "
-   (kbd (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) git)
+   (br)
+   (br)
+   (kbd (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) "git")
 	" clone git://git.savannah.nongnu.org/choicetool.git"))
 
 (h4 "Contribute")
