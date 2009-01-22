c4b2bf8b6024fe4ce1a968e3f3fb6b524c92fd5c
Francesco Salvestrini <salvestrini@gmail.com>
2009-01-22 19:05:18 +0100

    Updated site

--- site/in/index.scm
+++ site/in/index.scm
@@ -12,6 +12,46 @@
    (a (@ (href "http://en.wikipedia.org/wiki/Autotools")) autotools)
    ".")
 
+(h2 "INSTALLATION")
+
+(h3 "Prerequisites")
+(p "In order to install Choicetool into your system you will need:")
+(ul
+ (li (a (@ (href "http://www.perl.org"))               "Perl"     ) )
+ (li (a (@ (href "http://www.gnu.org/software/guile")) "Guile"    ) )
+ )
+
+(h3 "Installation")
+(p "The following notes do not override your system's package management "
+   "software, please follow your system's guidelines to install the software.")
+
+(p "Choicetool uses the autotools facilities, the simplest way to install "
+   "the package in your system is:")
+(ul
+ (li (kbd "cd") " to the directory containing the package's source code "
+     "and type " (kbd "./configure") " to configure the package for your "
+     "system."
+     "Running " (kbd "configure") " might take a while. While running, "
+     "it prints some messages telling which features it is checking for")
+ (li "Type " (kbd "make") " to compile the package")
+ (li "Optionally, type " (kbd "make check") " to run all self-tests that "
+     "come with the package")
+ (li "Type " (kbd "make install") " to install the programs and any data "
+     "files and documentation"))
+
+(p "Use " (kbd "./configure --help") " to get a brief help of all "
+   "configuration tweaks available.")
+(p "You can remove the program binaries and object files from the "
+   "source code directory by typing " (kbd "make clean") ". "
+   "To also remove the files that " (samp "configure") " created (so you "
+   "can compile the package for a different architecture), type "
+   (kbd "make distclean") ".")
+(p "You can also type " (kbd "make uninstall") " to remove the installed "
+   "files.")
+(p "Note that the " (samp "--prefix") " option allows installation using "
+   "different paths than the default ones, this option enables sandbox "
+   "installations.")
+
 (h3 "Use")
 (p "The complete input " (a (@ (href "./grammar.html")) "grammar") " "
    "is available.")
