d872d005a946fa1fdb2556215e96745a35818767
Francesco Salvestrini <salvestrini@gmail.com>
2009-01-22 22:41:18 +0100

    Updated site index

--- site/in/index.scm
+++ site/in/index.scm
@@ -5,54 +5,16 @@
    (a (@ (href "http://kernel.org/doc/#kconfig")) "Linux kernel kconfig")
    ".")
 (p "It uses an input description in order to know the configuration "
-   "symbols and their inter-relations and creates an outputs a script "
+   "symbols and their inter-relations and outputs a script "
    "that should be shipped with a package, in order to let the user tweak "
    "the configuration parameters interactively.")
 (p "Choicetool should work with (and even without) the "
    (a (@ (href "http://en.wikipedia.org/wiki/Autotools")) autotools)
    ".")
 
-(h2 "INSTALLATION")
+(h2 "USAGE")
 
-(h3 "Prerequisites")
-(p "In order to install Choicetool into your system you will need:")
-(ul
- (li (a (@ (href "http://www.perl.org"))               "Perl"     ) )
- (li (a (@ (href "http://www.gnu.org/software/guile")) "Guile"    ) )
- )
-
-(h3 "Installation")
-(p "The following notes do not override your system's package management "
-   "software, please follow your system's guidelines to install the software.")
-
-(p "Choicetool uses the autotools facilities, the simplest way to install "
-   "the package in your system is:")
-(ul
- (li (kbd "cd") " to the directory containing the package's source code "
-     "and type " (kbd "./configure") " to configure the package for your "
-     "system."
-     "Running " (kbd "configure") " might take a while. While running, "
-     "it prints some messages telling which features it is checking for")
- (li "Type " (kbd "make") " to compile the package")
- (li "Optionally, type " (kbd "make check") " to run all self-tests that "
-     "come with the package")
- (li "Type " (kbd "make install") " to install the programs and any data "
-     "files and documentation"))
-
-(p "Use " (kbd "./configure --help") " to get a brief help of all "
-   "configuration tweaks available.")
-(p "You can remove the program binaries and object files from the "
-   "source code directory by typing " (kbd "make clean") ". "
-   "To also remove the files that " (samp "configure") " created (so you "
-   "can compile the package for a different architecture), type "
-   (kbd "make distclean") ".")
-(p "You can also type " (kbd "make uninstall") " to remove the installed "
-   "files.")
-(p "Note that the " (samp "--prefix") " option allows installation using "
-   "different paths than the default ones, this option enables sandbox "
-   "installations.")
-
-(h3 "Use")
+(h3 "Input file grammar")
 (p "The complete input " (a (@ (href "./grammar.html")) "grammar") " "
    "is available.")
 
@@ -67,9 +29,6 @@
 (h2 "AUTHORS")
 (p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
 (p "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
-
-(h2 "RELEASES")
-(p "Sorry, no public release available at the moment.")
 
 (h2 "MAILING LISTS")
 (p "The project has a single moderated mailing list, with an archive. "
@@ -89,31 +48,3 @@
 (p "If you have a patch for a bug that hasn't yet been fixed in "
    "the latest repository sources, please be so kind to create it using the "
    "repository sources, not the release sources.")
-
-(h2 "DEVELOPMENT")
-
-(h3 "Browsing sources")
-(p "You can browse the "
-   (a (@ (href "http://git.savannah.nongnu.org/gitweb/?p=choicetool.git"))
-      "Git repository")
-   " of this project with your web browser. This gives you a good "
-   "picture of the current status of the source files. You may also view "
-   "the complete histories of any file in the repository as well as "
-   "differences among two versions.")
-
-(h3 "Getting a copy of the Git Repository")
-(p "Anonymous checkout:")
-(pre (@ class "terminal")
-     (a (@ (href "http://savannah.gnu.org/maintenance/UsingGit")) "git")
-     " clone git://git.savannah.nongnu.org/choicetool.git")
-
-(h3 "Contribute")
-(p "If you have time and programming skills, you can help us by developing "
-   "missing features, regression tests or bug-fixing the present codebase. "
-   "Subscribe to the "
-   (a (@ (href "http://lists.nongnu.org/mailman/listinfo/choicetool-generic"))
-      "mailing list")
-   ", drop us a mail and start coding. Send your code to the "
-   "mailing list under the form of patches for the current revision system.")
-(p "If you have time but no programming skills, you can help with "
-   "documentation,  packaging, tests before releases etc ...")
