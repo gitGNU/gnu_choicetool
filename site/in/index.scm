6fea4de2b2154d73cc5ee8bfd19a30e50214876f
Francesco Salvestrini <salvestrini@gmail.com>
2009-09-26 17:59:47 +0200

    Updated site generation

--- site/in/index.scm
+++ site/in/index.scm
@@ -24,11 +24,11 @@
       "GNU General Public License, version 2"))
 
 (h2 "MAINTAINERS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
+(p "Francesco Salvestrini")
 
 (h2 "AUTHORS")
-(p "Francesco Salvestrini <salvestrini AT gmail DOT com>")
-(p "Alessandro Massignan <ff0000 DOT it AT gmail DOT com>")
+(p "Francesco Salvestrini")
+(p "Alessandro Massignan")
 
 (h2 "MAILING LISTS")
 (p "The project has a single moderated mailing list, with an archive. "
@@ -41,7 +41,7 @@
 (h2 "REPORT A BUG")
 (p "If you think you have found a bug then please send as complete a report "
    "as possible to "
-   "<choicetool-generic AT nongnu DOT org>. "
+   "<" ,PACKAGE_BUGREPORT ">. "
    "An easy way to collect all the required information, such as platform and "
    "compiler, is to include in your report the config.log file available at "
    "the end of the configuration procedure.")
